6ef62d858d3fddaa95ae34a40a7950b9aa21ccc4
Veronica De Felice <verodefelic@gmail.com>
2023-06-15 16:49:30 -0400

    updated modal with email contact

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,18 +1,13 @@
 import { Container,Row,Col} from "react-bootstrap"
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
  export default function Main() {
-  const values = [true];
-  const [fullscreen, setFullscreen] = useState(true);
-  const [show, setShow] = useState(false);
+  const [lgShow, setLgShow] = useState(false);
+ 
 
-  function handleShow(breakpoint) {
-    setFullscreen(breakpoint);
-    setShow(true);
-  }
-    
+
 
     return (
         
@@ -22,21 +17,22 @@ import Modal from 'react-bootstrap/Modal';
                     </Col>
                     <Col className="intro">
                      <h1>Hello, I'm Veronica, welcome to my page! Here, you'll discover a collection of my work and learn a few things about me.</h1>
-                     <>
-                          {values.map((v, idx) => (
-                            <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
-                              More...
-                              {typeof v === 'string' && `below ${v.split('-')[0]}`}
-                            </Button>
-                          ))}
-
-                          <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-                            <Modal.Header closeButton>
-                              <Modal.Title>Title</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>Description</Modal.Body>
-                          </Modal>
-                    </>
+                      <>
+                        <Button onClick={() => setLgShow(true)}>Contact Info</Button>
+                        <Modal
+                          size="lg"
+                          show={lgShow}
+                          onHide={() => setLgShow(false)}
+                          aria-labelledby="example-modal-sizes-title-lg"
+                        >
+                          <Modal.Header closeButton>
+                            <Modal.Title id="example-modal-sizes-title-lg">
+                              Email Me @:
+                            </Modal.Title>
+                          </Modal.Header>
+                          <Modal.Body>verodefelic@gmail.com</Modal.Body>
+                        </Modal>
+                      </>
                    </Col>
                 </Row>
             </Container>
