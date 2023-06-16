22ce0f993e71d323639694ed509204f97e8a4961
Veronica De Felice <verodefelic@gmail.com>
2023-06-15 23:17:54 -0400

    added clickable image for each json pulling site from data

--- src/layout/Gallery/Panel.jsx
+++ src/layout/Gallery/Panel.jsx
@@ -1,10 +1,12 @@
 import { Col, Image } from "react-bootstrap"
-export default function Panel ({ element: {title, image }}) {
+export default function Panel ({ element: {title, image,site }}) {
 
     return(
         <Col xs={11} md={4} lg={3}>
-            <Image  src={`/images/${image}`} fluid/>
-            <h3>{title}</h3>
+              <a href={site} target="_blank" rel="noreferrer">
+               <Image src={`/images/${image}`} fluid/>
+              </a>
+              <h3>{title}</h3>
         </Col>
     )
 }
--- src/layout/NavMenu/NavMenu.jsx
+++ src/layout/NavMenu/NavMenu.jsx
@@ -1,12 +1,27 @@
 import { Navbar,Container,Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
-export default function NavMenu () {
 
+
+
+export default function NavMenu () {
+  const gitHubLink = "https://github.com/veronicadfl/profile-app";
+  const Linkedinlink ="https://www.linkedin.com/in/veronicadefelice/"
     return (
 
         <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
+       
+                 <a href={gitHubLink} 
+                 target="_blank"
+                 rel="noreferrel" >GitHub</a>
+                  <br/>
+                <a href={Linkedinlink} 
+                 target="_blank"
+                 rel="noreferrel">Linkedln</a>
+                 <br/>
+       
+       
           <Navbar.Brand as={Link} to="/" href="#home">VERONICA DE FELICE</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
